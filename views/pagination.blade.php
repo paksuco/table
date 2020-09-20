514f9ef161a71f0c1c093d4d40f934f53b6c04aa
Taha Paksu <tpaksu@gmail.com>
2020-09-20 23:25:36 +0300

    fix pagination links

--- views/pagination.blade.php
+++ views/pagination.blade.php
@@ -6,15 +6,15 @@
     <ul class="flex flex-1 justify-end" role="navigation">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <li class="px-5 py-2 shadow rounded-l-lg bg-white text-gray-300" aria-text-gray-300="true" aria-label="@lang('pagination.previous')">
-                <span class="page-link" aria-hidden="true">
+            <li class="shadow rounded-l-lg bg-white text-gray-300" aria-text-gray-300="true" aria-label="@lang('pagination.previous')">
+                <span class="page-link block px-4 py-2" aria-hidden="true">
                     <span class="hidden md:block">&lsaquo;</span>
                     <span class="block md:hidden">@lang('pagination.previous')</span>
                 </span>
             </li>
         @else
-            <li class="px-5 py-2 shadow rounded-l-lg bg-white">
-                <button type="button" class="page-link" wire:click="previousPage" rel="prev" aria-label="@lang('pagination.previous')">
+            <li class="shadow rounded-l-lg bg-white">
+                <button type="button" class="page-link px-4 py-2 " wire:click="previousPage" rel="prev" aria-label="@lang('pagination.previous')">
                     <span class="hidden md:block">&lsaquo;</span>
                     <span class="block md:hidden">@lang('pagination.previous')</span>
                 </button>
@@ -25,16 +25,16 @@
         @foreach ($elements as $element)
             {{-- "Three Dots" Separator --}}
             @if (is_string($element))
-                <li class="px-3 py-2 mx-0 text-gray-300 hidden md:block" aria-text-gray-300="true"><span class="page-link">{{ $element }}</span></li>
+                <li class="mx-0 text-gray-300 hidden md:block" aria-text-gray-300="true"><span class="page-link block px-3 py-2">{{ $element }}</span></li>
             @endif
 
             {{-- Array Of Links --}}
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <li class="px-5 py-2 bg-blue-600 shadow text-white hidden md:block" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                        <li class=" bg-blue-600 shadow text-white hidden md:block" aria-current="page"><span class="page-link block px-4 py-2">{{ $page }}</span></li>
                     @else
-                        <li class="px-5 py-2 shadow bg-white hidden md:block"><button type="button" class="page-link" wire:click="gotoPage({{ $page }})">{{ $page }}</button></li>
+                        <li class="shadow bg-white hidden md:block"><button type="button" class="page-link px-4 py-2" wire:click="gotoPage({{ $page }})">{{ $page }}</button></li>
                     @endif
                 @endforeach
             @endif
@@ -42,15 +42,15 @@
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <li class="px-5 py-2 shadow rounded-r-lg bg-white ">
-                <button type="button" class="page-link" wire:click="nextPage" rel="next" aria-label="@lang('pagination.next')">
+            <li class="shadow rounded-r-lg bg-white ">
+                <button type="button" class="page-link px-4 py-2" wire:click="nextPage" rel="next" aria-label="@lang('pagination.next')">
                     <span class="block md:hidden">@lang('pagination.next')</span>
                     <span class="hidden md:block">&rsaquo;</span>
                 </button>
             </li>
         @else
-            <li class="px-5 py-2 shadow rounded-r-lg bg-white text-gray-300" aria-text-gray-300="true" aria-label="@lang('pagination.next')">
-                <span class="page-link" aria-hidden="true">
+            <li class="shadow rounded-r-lg bg-white text-gray-300" aria-text-gray-300="true" aria-label="@lang('pagination.next')">
+                <span class="page-link block px-5 py-2" aria-hidden="true">
                     <span class="block md:hidden">@lang('pagination.next')</span>
                     <span class="hidden md:block">&rsaquo;</span>
                 </span>
