a26f890bc7bbaadd1eebaeb97cfc76220b236784
Taha Paksu <tpaksu@gmail.com>
2021-01-25 03:59:24 +0300

    Translation Continues

--- views/components/filters.blade.php
+++ views/components/filters.blade.php
@@ -1,11 +1,11 @@
-<div class="mb-4 flex justify-between items-center">
+<div class="flex items-center justify-between mb-4">
     @if($settings->queryable)
     <div class="flex-1 pr-4">
         <div class="relative md:w-1/3">
             <input type="search" wire:model.debounce.500ms="settings.query"
-                class="w-full pl-8 pr-4 py-2 rounded border shadow-sm focus:outline-none focus:shadow-outline text-gray-600 font-medium"
+                class="w-full py-2 pl-8 pr-4 font-medium text-gray-600 border rounded shadow-sm focus:outline-none focus:shadow-outline"
                 placeholder="Search...">
-            <div class="absolute left-2 inset-y-0 inline-flex justify-center items-center">
+            <div class="absolute inset-y-0 inline-flex items-center justify-center left-2">
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-400" viewBox="0 0 24 24"
                     stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
                     stroke-linejoin="round">
@@ -21,11 +21,10 @@
     @endif
     <div class="flex">
         @if($settings->pageable)
-        <div class="shadow-sm rounded border flex mr-3" x-data="{ open: false }">
+        <div class="flex mr-3 border rounded shadow-sm" x-data="{ open: false }">
             <div class="relative">
                 <button @click.prevent="open = !open"
-                    class="rounded inline-flex items-center bg-white hover:text-blue-500 focus:outline-none
-                        focus:shadow-outline text-gray-500 font-semibold py-2 px-2 md:px-4">
+                    class="inline-flex items-center px-2 py-2 font-semibold text-gray-500 bg-white rounded hover:text-blue-500 focus:outline-none focus:shadow-outline md:px-4">
                     <span>{{$settings->perPage}}</span>
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ml-1" width="24" height="24"
                         viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
@@ -36,7 +35,7 @@
                 </button>
 
                 <div x-show="open" @click.away="open = false"
-                    class="z-40 absolute top-0 right-0 bg-white w-16 rounded shadow-sm border mt-12 -mr-1 block py-1 overflow-hidden">
+                    class="absolute top-0 right-0 z-40 block w-16 py-1 mt-12 -mr-1 overflow-hidden bg-white border rounded shadow-sm">
                     @foreach($settings->perPageOptions as $perPageOption)
                     <label
                         class="@if($perPageOption == $settings->perPage) bg-blue-600 text-white @else
@@ -51,10 +50,10 @@
             </div>
         </div>
         @endif
-        <div class="shadow-sm border rounded flex" x-data="{ open: false, parent: datatables() }">
+        <div class="flex border rounded shadow-sm" x-data="{ open: false, parent: datatables() }">
             <div class="relative">
                 <button @click.prevent="open = !open"
-                    class="rounded inline-flex items-center bg-white hover:text-blue-500 focus:outline-none focus:shadow-outline text-gray-500 font-semibold py-2 px-2 md:px-4">
+                    class="inline-flex items-center px-2 py-2 font-semibold text-gray-500 bg-white rounded hover:text-blue-500 focus:outline-none focus:shadow-outline md:px-4">
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 md:hidden" viewBox="0 0 24 24"
                         stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
                         stroke-linejoin="round">
@@ -62,7 +61,7 @@
                         <path
                             d="M5.5 5h13a1 1 0 0 1 0.5 1.5L14 12L14 19L10 16L10 12L5 6.5a1 1 0 0 1 0.5 -1.5" />
                     </svg>
-                    <span class="hidden md:block">Display</span>
+                    <span class="hidden md:block">@lang('Display')</span>
                     <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ml-1" width="24" height="24"
                         viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
                         stroke-linecap="round" stroke-linejoin="round">
@@ -72,16 +71,16 @@
                 </button>
 
                 <div x-show="open" @click.away="open = false"
-                    class="z-40 absolute top-0 right-0 w-40 bg-white rounded border shadow-sm mt-12 -mr-1 block py-1 overflow-hidden">
+                    class="absolute top-0 right-0 z-40 block w-40 py-1 mt-12 -mr-1 overflow-hidden bg-white border rounded shadow-sm">
                     @foreach(collect($settings->fields)->pluck("name") as $header)
                     <label
-                        class="flex justify-start items-center text-truncate hover:bg-gray-100 px-4 py-2">
-                        <div class="text-teal-600 mr-3">
+                        class="flex items-center justify-start px-4 py-2 text-truncate hover:bg-gray-100">
+                        <div class="mr-3 text-teal-600">
                             <input type="checkbox"
                                 class="form-checkbox focus:outline-none focus:shadow-outline" checked
                                 @click="parent.toggleColumn('{{$header}}', $event)">
                         </div>
-                        <div class="select-none text-gray-700" x-text="'{{$header}}'"></div>
+                        <div class="text-gray-700 select-none" x-text="'{{$header}}'"></div>
                     </label>
                     @endforeach
                 </div>
--- views/pagination.blade.php
+++ views/pagination.blade.php
@@ -1,20 +1,24 @@
 @if ($paginator->hasPages())
 <div class="flex items-start mt-4">
-    <div class="items-center justify-center mr-5 text-gray-700 hidden md:block">
-        <div>Showing <span class="font-semibold">{{$paginator->firstItem()}}</span> to <span class="font-semibold">{{$paginator->lastItem()}}</span> of <span class="font-semibold">{{$paginator->total()}}</span> items</div>
+    <div class="items-center justify-center hidden mr-5 text-gray-700 md:block">
+        <div>@lang("Showing :start to :end of :count items", [
+            "start" => '<span class="font-semibold">' . $paginator->firstItem() . '</span>',
+            "end" => '<span class="font-semibold">'. $paginator->lastItem() . '</span>',
+            "count" => '<span class="font-semibold">' . $paginator->total() . '</span>'
+        ])</div>
     </div>
-    <ul class="flex flex-1 justify-end" role="navigation">
+    <ul class="flex justify-end flex-1" role="navigation">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <li class="shadow rounded-l-lg bg-white text-gray-300" aria-text-gray-300="true" aria-label="@lang('pagination.previous')">
-                <span class="page-link block px-4 py-2" aria-hidden="true">
+            <li class="text-gray-300 bg-white rounded-l-lg shadow" aria-text-gray-300="true" aria-label="@lang('pagination.previous')">
+                <span class="block px-4 py-2 page-link" aria-hidden="true">
                     <span class="hidden md:block">&lsaquo;</span>
                     <span class="block md:hidden">@lang('pagination.previous')</span>
                 </span>
             </li>
         @else
-            <li class="shadow rounded-l-lg bg-white">
-                <button type="button" class="page-link px-4 py-2 " wire:click="previousPage" rel="prev" aria-label="@lang('pagination.previous')">
+            <li class="bg-white rounded-l-lg shadow">
+                <button type="button" class="px-4 py-2 page-link " wire:click="previousPage" rel="prev" aria-label="@lang('pagination.previous')">
                     <span class="hidden md:block">&lsaquo;</span>
                     <span class="block md:hidden">@lang('pagination.previous')</span>
                 </button>
@@ -25,16 +29,16 @@
         @foreach ($elements as $element)
             {{-- "Three Dots" Separator --}}
             @if (is_string($element))
-                <li class="mx-0 text-gray-300 hidden md:block" aria-text-gray-300="true"><span class="page-link block px-3 py-2">{{ $element }}</span></li>
+                <li class="hidden mx-0 text-gray-300 md:block" aria-text-gray-300="true"><span class="block px-3 py-2 page-link">{{ $element }}</span></li>
             @endif
 
             {{-- Array Of Links --}}
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <li class=" bg-blue-600 shadow text-white hidden md:block" aria-current="page"><span class="page-link block px-4 py-2">{{ $page }}</span></li>
+                        <li class="hidden text-white bg-blue-600 shadow  md:block" aria-current="page"><span class="block px-4 py-2 page-link">{{ $page }}</span></li>
                     @else
-                        <li class="shadow bg-white hidden md:block"><button type="button" class="page-link px-4 py-2" wire:click="gotoPage({{ $page }})">{{ $page }}</button></li>
+                        <li class="hidden bg-white shadow md:block"><button type="button" class="px-4 py-2 page-link" wire:click="gotoPage({{ $page }})">{{ $page }}</button></li>
                     @endif
                 @endforeach
             @endif
@@ -42,15 +46,15 @@
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <li class="shadow rounded-r-lg bg-white ">
-                <button type="button" class="page-link px-4 py-2" wire:click="nextPage" rel="next" aria-label="@lang('pagination.next')">
+            <li class="bg-white rounded-r-lg shadow ">
+                <button type="button" class="px-4 py-2 page-link" wire:click="nextPage" rel="next" aria-label="@lang('pagination.next')">
                     <span class="block md:hidden">@lang('pagination.next')</span>
                     <span class="hidden md:block">&rsaquo;</span>
                 </button>
             </li>
         @else
-            <li class="shadow rounded-r-lg bg-white text-gray-300" aria-text-gray-300="true" aria-label="@lang('pagination.next')">
-                <span class="page-link block px-5 py-2" aria-hidden="true">
+            <li class="text-gray-300 bg-white rounded-r-lg shadow" aria-text-gray-300="true" aria-label="@lang('pagination.next')">
+                <span class="block px-5 py-2 page-link" aria-hidden="true">
                     <span class="block md:hidden">@lang('pagination.next')</span>
                     <span class="hidden md:block">&rsaquo;</span>
                 </span>
